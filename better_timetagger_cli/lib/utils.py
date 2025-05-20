e5c1624b857939f70d3015ce2689a026e03f3acd
Paul Bütof <paul@passionate-bytes.com>
2025-05-20 09:54:40 +0200

    Add utilities

--- better_timetagger_cli/lib/utils.py
+++ better_timetagger_cli/lib/utils.py
@@ -1,8 +1,12 @@
 import os
 import subprocess
 import sys
+from datetime import datetime
+from time import time
 
 import click
+
+from .types import Record
 
 
 def open_file(path: str, editor: str | None = None) -> None:
@@ -69,3 +73,111 @@ def user_config_dir(appname=None, roaming=False) -> str:
             os.makedirs(path, exist_ok=True)
 
     return str(path)
+
+
+def total_time(records: list[Record], start: datetime, end: datetime) -> int:
+    """
+    Calculate the total time spent on records within a given time range.
+
+    Args:
+        records: A list of records, each containing 't1' and 't2' timestamps.
+        start: The start datetime of the time range.
+        end: The end datetime of the time range.
+
+    Returns:
+        The total time in seconds spent on the records within the time range.
+    """
+    total = 0
+    t_start = start.timestamp()
+    t_end = end.timestamp()
+    t_now = datetime.now().timestamp()
+    for r in records:
+        t1 = r["t1"]
+        t2 = r["t2"] if r["t1"] != r["t2"] else t_now
+        total += min(t_end, t2) - max(t_start, t1)
+    return total
+
+
+def readable_time(timestamp) -> str:
+    """
+    Turn a timestamp into a readable string.
+
+    Args:
+        timestamp: The timestamp to convert.
+
+    Returns:
+        A string representing the timestamp in 'YYYY-MM-DD HH:MM' format.
+    """
+    value = datetime.fromtimestamp(timestamp)
+    return f"{value:%Y-%m-%d %H:%M}"
+
+
+def readable_duration(nsecs) -> str:
+    """
+    Turn a duration in seconds into a reabable string.
+
+    Args:
+        nsecs: The duration in seconds.
+
+    Returns:
+        A string representing the duration in 'HH:MM' format.
+    """
+    m = round(nsecs / 60)
+    return f"{m // 60:.0f}:{m % 60:02.0f}"
+
+
+def print_records(records: list[Record]) -> None:
+    """
+    Pretty-print a list of records.
+
+    Args:
+        records: A list of records.
+    """
+    # Sort
+    records = sorted(records, key=lambda r: r["t1"])
+
+    # Write header
+    print(f"{'Started':>17} {'Stopped':>17} {'Duration':>9}  Description")
+
+    # Write each record
+    for r in records:
+        started = readable_time(r["t1"])
+        if r["t1"] == r["t2"]:
+            stopped = "-"
+            duration = readable_duration(time() - r["t1"])
+        else:
+            stopped = readable_time(r["t2"])
+            duration = readable_duration(r["t2"] - r["t1"])
+            if stopped.split(" ")[0] == started.split(" ")[0]:
+                stopped = stopped.split(" ", 1)[1]
+        description = " " + r.get("ds", "")
+        click.echo(f"{started:>17} {stopped:>17} {duration:>9} {description}")
+
+
+def print_records_csv(records: list[Record]) -> None:
+    """
+    Print records in CSV format.
+
+    Args:
+        records: A list of records.
+    """
+    # Sort
+    records = sorted(records, key=lambda r: r["t1"])
+
+    # Write header
+    print("Started,Stopped,Duration,Description")
+
+    # Write each record
+    for r in records:
+        started = readable_time(r["t1"])
+        if r["t1"] == r["t2"]:
+            stopped = ""
+            duration = readable_duration(time() - r["t1"])
+
+        else:
+            stopped = readable_time(r["t2"])
+            duration = readable_duration(r["t2"] - r["t1"])
+            if stopped.split(" ")[0] == started.split(" ")[0]:
+                stopped = stopped.split(" ", 1)[1]
+        description = " " + r.get("ds", "")
+        click.echo(f"{started},{stopped},{duration},{description}")
