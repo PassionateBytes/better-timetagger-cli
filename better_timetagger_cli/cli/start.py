4e03f23e431a427612e9fe002f0af4193ed93fc5
Paul Bütof <paul@passionate-bytes.com>
2025-05-21 12:21:31 +0200

    Add better tag handling to start command

--- better_timetagger_cli/cli/start.py
+++ better_timetagger_cli/cli/start.py
@@ -14,14 +14,20 @@ from better_timetagger_cli.lib.utils import generate_uid, print_records
     is_flag=True,
     help="Keep previous tasks running, do not stop them.",
 )
+@click.option(
+    "-t",
+    "--tag",
+    type=click.STRING,
+    multiple=True,
+    help="Apply tags to the task. Can be used multiple times. Alternatively, add tags using '#' in the description.",
+)
 @click.argument("description", type=click.STRING, nargs=-1)
-def start(keep: bool, description: list[str] | str) -> None:
+def start(keep: bool, tag: list[str], description: list[str]) -> None:
     """
     Start timer with the given description.
-    Use hashes ('#') in your description to add tags.
     """
-    if not isinstance(description, str):
-        description = " ".join(description)
+    tags = [f"#{t}" if not t.startswith("#") else t for t in tag]
+    description_string = f"{' '.join(description)} {' '.join(tags)}"
 
     now = int(time())
     new_record = {
@@ -30,7 +36,7 @@ def start(keep: bool, description: list[str] | str) -> None:
         "t2": now,
         "mt": now,
         "st": 0,
-        "ds": description,
+        "ds": description_string,
     }
     running_records = get_runnning_records()["records"]
 
@@ -40,7 +46,7 @@ def start(keep: bool, description: list[str] | str) -> None:
 
     else:
         for r in running_records:
-            if r.get("ds", "") == description:
+            if r.get("ds", "") == description_string:
                 abort("Timer with this description is already running.")
             r["t2"] = now
         put_records([new_record, *running_records])
