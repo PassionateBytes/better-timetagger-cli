18f82df7e70344589517fcbea7aee340f91e4b20
Paul Bütof <paul@passionate-bytes.com>
2025-05-24 09:46:26 +0200

    Implement initial test

--- better_timetagger_cli/__init__.py
+++ better_timetagger_cli/__init__.py
@@ -1,8 +1,10 @@
 # set __version__ dynamically
-# fmt:off
 def __getattr__(name):
     if name == "__version__":
         from importlib.metadata import version
+
         return version(__name__)
     return super().__getattr__(name)
-# fmt:on
+
+
+__version__: str
