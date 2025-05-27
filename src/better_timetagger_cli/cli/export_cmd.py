0947d5320ab5c8c2179682525a39ad842c32571d
Paul Bütof <paul.buetof@passionate-bytes.com>
2025-05-27 02:24:41 +0200

    Implement filter options from show command in export command

--- src/better_timetagger_cli/cli/export_cmd.py
+++ src/better_timetagger_cli/cli/export_cmd.py
@@ -2,7 +2,7 @@ from typing import Literal
 
 import click
 
-from better_timetagger_cli.lib.api import get_records
+from better_timetagger_cli.lib.api import get_records, get_running_records
 from better_timetagger_cli.lib.console import console
 from better_timetagger_cli.lib.misc import abort
 from better_timetagger_cli.lib.parsers import parse_start_end, tags_callback
@@ -36,6 +36,18 @@ from better_timetagger_cli.lib.records import records_to_csv
     help="Include records earlier than this time. Supports natural language.",
 )
 @click.option(
+    "-H",
+    "--hidden",
+    is_flag=True,
+    help="List only hidden (i.e. removed) records in the output.",
+)
+@click.option(
+    "-r",
+    "--running",
+    is_flag=True,
+    help="List running records in the output.",
+)
+@click.option(
     "-x",
     "--match",
     "tags_match",
@@ -48,6 +60,8 @@ def export_cmd(
     file: click.File | None,
     start: str | None,
     end: str | None,
+    hidden: bool,
+    running: bool,
     tags_match: Literal["any", "all"],
 ) -> None:
     """
@@ -59,16 +73,29 @@ def export_cmd(
     The parameters '--start' and '--end' support natural language to specify date and time.
     You can use phrases like 'yesterday', 'June 11', '5 minutes ago', or '05/12 3pm'.
     """
+    if running and (start or end):
+        abort("The '--running' option cannot be used with '--start' or '--end'.")
+
     start_dt, end_dt = parse_start_end(start, end)
 
-    records = get_records(
-        start_dt,
-        end_dt,
-        tags=tags,
-        tags_match=tags_match,
-        sort_by="t1",
-        sort_reverse=False,
-    )["records"]
+    if not running:
+        records = get_records(
+            start_dt,
+            end_dt,
+            tags=tags,
+            tags_match=tags_match,
+            sort_by="t1",
+            sort_reverse=False,
+            hidden=hidden,
+        )["records"]
+    else:
+        records = get_running_records(
+            tags=tags,
+            tags_match=tags_match,
+            sort_by="t1",
+            sort_reverse=False,
+            hidden=hidden,
+        )["records"]
 
     if not records:
         abort("No records found.")
