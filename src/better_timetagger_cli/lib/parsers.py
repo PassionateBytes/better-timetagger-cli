87ad00ad38cb2f00e57d5f1c29a630099ddbdd12
Paul Bütof <paul.buetof@passionate-bytes.com>
2025-06-09 14:44:47 +0200

    Update parsing logic to use new `parsedatetime` dependency

--- src/better_timetagger_cli/lib/parsers.py
+++ src/better_timetagger_cli/lib/parsers.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 import click
-import dateparser
+import parsedatetime  # type: ignore[import-untyped]
 
 from .misc import abort
 
@@ -23,6 +23,37 @@ def tags_callback(ctx: click.Context, param: click.Parameter, tags: list[str]) -
     return tags
 
 
+def parse_datetime(input: str) -> datetime | None:
+    """
+    Parse nartural language date and time input.
+
+    Args:
+        input: A string representing the date and time.
+
+    Returns:
+        A datetime object representing the parsed date and time.
+    """
+
+    cal = parsedatetime.Calendar()
+    time_struct, parse_status = cal.parse(input)
+
+    # only date parsed -> set time to 00:00:00
+    if parse_status == 1:
+        return datetime(*time_struct[:3])
+
+    # only time parsed -> set date to today
+    elif parse_status == 2:
+        today = datetime.today()
+        return datetime(today.year, today.month, today.day, *time_struct[:3])
+
+    # date and time parsed
+    elif parse_status == 3:
+        return datetime(*time_struct[:6])
+
+    else:
+        return None
+
+
 def parse_start_end(
     start: str | None,
     end: str | None,
@@ -37,8 +68,8 @@ def parse_start_end(
     Returns:
         A tuple containing the start and end date and time.
     """
-    start_dt = dateparser.parse(start) if start is not None else datetime(2000, 1, 1)
-    end_dt = dateparser.parse(end) if end is not None else datetime(3000, 1, 1)
+    start_dt = parse_datetime(start) if start else datetime(2000, 1, 1)
+    end_dt = parse_datetime(end) if end else datetime(3000, 1, 1)
 
     if start_dt is None:
         abort("Could not parse '--start'.")
@@ -59,7 +90,7 @@ def parse_at(at: str | None) -> int | None:
         The parsed start time as a timestamp, or None if not provided.
     """
     if at:
-        at_dt = dateparser.parse(at)
+        at_dt = parse_datetime(at)
         if not at_dt:
             abort("Could not parse '--at'.")
         return int(at_dt.timestamp())
